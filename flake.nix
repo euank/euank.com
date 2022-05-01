0342ad74f9049d7a123b1c1275c05601d4b8d21f
Euan Kemp <euank@euank.com>
2022-05-01 04:18:14 -0700

    nix flake update

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "Flake to build the site image";
 
   outputs = { self, nixpkgs }:
   let image = import ./image.nix { inherit nixpkgs; }; in {
